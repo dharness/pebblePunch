2d8a2eca697762c7c13811d5304e8c308d63f2a1
Dylan Harness <dharness@uwo.ca>
2015-01-31 05:46:53 -0500

    ui

--- app.js
+++ app.js
@@ -10,48 +10,66 @@ var Accel = require('ui/accel');
 var ajax = require('ajax');
 Accel.init();
 
+// Make a list of menu items
+var mitems = [{
+    title: "Punch!",
+    subtitle: "Giver 'er"
+}, {
+    title: "High Score",
+    subtitle: "Compete!"
+}];
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello Dylan!',
-  body: 'Press any button.'
+// Create the Menu, supplying the list of fruits
+var MainMenu = new UI.Menu({
+    sections: [{
+        title: 'Menu',
+        items: mitems
+    }]
 });
 
-main.show();
+MainMenu.show();
+
+// Add a click listener for select button click
+MainMenu.on('select', function(event) {
+
+    // Show a card with clicked item details
+    var detailCard = new UI.Card({
+        title: 'Punch!'
+    });
+
+    // Show the new Card
+    detailCard.show();
+});
 
 Accel.on('tap', function(e) {
-  
-  Accel.peek(function(e) {
-    
-    var sendData = {
-      magX : e.accel.x,
-      magY: e.accel.y,
-      magZ: e.accel.z
-    };
-    
-    console.log(JSON.stringify(magnitudes));
-    
-    ajax(
-      {
-        url: 'http://104.131.88.222:6113',
-        method: 'POST',
-        data: sendData,
-        type: 'json',
-        contentType: "application/json; charset=utf-8",
-      },
-      function(data) {
-        // Success!
-        console.log('Successfully fetched weather data!');
-      },
-      function(error) {
-        // Failure!
-        console.log('Failed fetching weather data: ' + error);
-      }
-    );
-      
-  });
-  
+
+    Accel.peek(function(e) {
+
+        var sendData = {
+            magX: e.accel.x,
+            magY: e.accel.y,
+            magZ: e.accel.z
+        };
+
+        console.log(JSON.stringify(magnitudes));
+
+        ajax({
+                url: 'http://104.131.88.222:6113',
+                method: 'POST',
+                data: sendData,
+                type: 'json',
+                contentType: "application/json; charset=utf-8",
+            },
+            function(data) {
+                // Success!
+                console.log('Successfully fetched weather data!');
+            },
+            function(error) {
+                // Failure!
+                console.log('Failed fetching weather data: ' + error);
+            }
+        );
+
+    });
+
 });
-
-
--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ var UI = require('ui');
 var Vector2 = require('vector2');
 var Accel = require('ui/accel');
 var ajax = require('ajax');
+var Vibe = require('ui/vibe');
 Accel.init();
 
 // -------------------- Engine -------------------- 
@@ -15,41 +16,65 @@ Accel.init();
 // A computational engine
 var Engine = {
 
-	//takes an array of data and determines if a punch has occurred
-	lookForPunch: function(accels) {
-		var diff = 20; // the difference which defines a peak
-		var threshHold = 100; // the minimum magnitude for a punch
+    //takes an array of data and determines if a punch has occurred
+    lookForPunch: function(accels) {
+        var diff = 20; // the difference which defines a peak
+        var threshHold = 100; // the minimum magnitude for a punch
 
-		for (var i =0; i < accels.length; i++){
-			var a = accels[i]; // grab the current acceloration
-			console.log (a.x + ' ' +a.y +' ' + a.z)
-			var m = Math.sqrt(a.x*a.x + a.y*a.y + a.z*a.z); //compute the magnitude of the vector
-			console.log('magnitude: ------> ' + m);
-		}
-	}
+        for (var i = 0; i < accels.length; i++) {
+            var a = accels[i]; // grab the current acceloration
+            console.log(a.x + ' ' + a.y + ' ' + a.z)
+            var m = Math.sqrt(a.x * a.x + a.y * a.y + a.z * a.z); //compute the magnitude of the vector
+            console.log('magnitude: ------> ' + m);
+        }
+    }
 }
 
 
 // -------------------- UI -------------------- 
 
-var main = new UI.Card({
-	title: 'Pebble.js',
-	icon: 'images/menu_icon.png',
-	subtitle: 'Hello Dylan!',
-	body: 'Press any button.'
+// Make a list of menu items
+var mitems = [{
+    title: "Punch!",
+    subtitle: "Giver 'er"
+}, {
+    title: "High Score",
+    subtitle: "Compete!"
+}];
+
+// Create the Menu, supplying the list of fruits
+var MainMenu = new UI.Menu({
+    sections: [{
+        title: 'Menu',
+        items: mitems
+    }]
 });
 
-main.show();
+MainMenu.show();
+
+// Add a click listener for select button click
+MainMenu.on('select', function(event) {
+    Vibe.vibration('short');
+
+    // Show a card with clicked item details
+    var detailCard = new UI.Card({
+        title: 'Punch!',
+        icon: 'resources/images/punch.png.pbi'
+    });
+
+    // Show the new Card
+    detailCard.show();
+});
 
 // -------------------- ACCELOROMETER --------------------
 
 Accel.config({
-	rate: 25,
-	samples: 25,
-	subscribe: true,
+    rate: 25,
+    samples: 25,
+    subscribe: true,
 });
 
 // subscription listening for punches
 Accel.on('data', function(e) {
-	Engine.lookForPunch(e.accels);
+    Engine.lookForPunch(e.accels);
 });
